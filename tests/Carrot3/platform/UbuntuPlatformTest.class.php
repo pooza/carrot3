1a6dd7d0fecb9dbe3ace690f5623de02b4f07b52
Tatsuya Koishi <tkoishi@b-shock.co.jp>
2017-12-11 19:26:13 +0900

    refs #91

--- tests/Carrot3/platform/UbuntuPlatformTest.class.php
+++ tests/Carrot3/platform/UbuntuPlatformTest.class.php
@@ -5,10 +5,10 @@ namespace Carrot3;
  * @package jp.co.b-shock.carrot3
  * @author 小石達也 <tkoishi@b-shock.co.jp>
  */
-class DarwinPlatformTest extends Test {
+class UbuntuPlatformTest extends Test {
 	public function execute () {
-		$platform = Platform::create('darwin');
+		$platform = Platform::create('ubuntu');
 		$this->assert('create', $platform instanceof Platform);
-		$this->assert('getProcessOwner', $platform->getProcessOwner() == 'www');
+		$this->assert('getProcessOwner', $platform->getProcessOwner() == 'www-data');
 	}
 }
