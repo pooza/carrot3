ef8b22343ddb8e35803aac23a6fb2bf2a5ff6fee
Tatsuya Koishi <tkoishi@b-shock.co.jp>
2018-04-14 23:22:22 +0900

    fixes #161

--- lib/Carrot3/KeyGenerator.trait.php
+++ lib/Carrot3/KeyGenerator.trait.php
@@ -16,14 +16,16 @@ trait KeyGenerator {
 	 * シリアライズのダイジェストを返す
 	 *
 	 * @access protected
-	 * @param iterable $values 属性名に用いる値
+	 * @param mixed $values 属性名に用いる値
 	 * @return string 属性名
 	 */
-	protected function createKey (iterable $values):string {
+	protected function createKey ($values):string {
+		if ($values instanceof Serializable) {
+			$values = $values->digest();
+		}
 		$values = Tuple::create($values);
 		$values[] = Controller::getInstance()->getHost()->getName();
 		$values[] = Utils::getClass($this);
-		$values[] = __FUNCTION__;
 		return Crypt::digest(
 			(new PHPSerializer)->encode($values->decode())
 		);
