6de623d52d71a99b2e245afafaa5d0b84e4316b2
Tatsuya Koishi <tkoishi@b-shock.co.jp>
2018-04-20 09:24:49 +0900

    #172

--- lib/Carrot3/view/renderer/smarty/plugins/function.html_options.php
+++ lib/Carrot3/view/renderer/smarty/plugins/function.html_options.php
@@ -12,38 +12,38 @@ use \Carrot3 as C;
  * @author 小石達也 <tkoishi@b-shock.co.jp>
  */
 function smarty_function_html_options ($params, &$smarty) {
-    $params = C\Tuple::create($params);
-    $select = new C\HTMLElement('select');
-    $select->setID($params['id']);
-    $select->setAttribute('name', $params['name']);
-    if ($params['options']) {
-        foreach ($params['options'] as $key => $value) {
-            $option = $select->createElement('option');
-            $option->setAttribute('value', $key);
-            $option->setAttribute('label', $value);
-        }
-    } else if ($params['values'] && $params['output']) {
-        foreach ($params['values'] as $index => $value) {
-            $option = $select->createElement('option');
-            $option->setAttribute('value', $value);
-            $option->setAttribute('label', $params['output'][$index]);
-        }
-    }
+	$params = C\Tuple::create($params);
+	$select = new C\HTMLElement('select');
+	$select->setID($params['id']);
+	$select->setAttribute('name', $params['name']);
+	if ($params['options']) {
+		foreach ($params['options'] as $key => $value) {
+			$option = $select->createElement('option');
+			$option->setAttribute('value', $key);
+			$option->setAttribute('label', $value);
+		}
+	} else if ($params['values'] && $params['output']) {
+		foreach ($params['values'] as $index => $value) {
+			$option = $select->createElement('option');
+			$option->setAttribute('value', $value);
+			$option->setAttribute('label', $params['output'][$index]);
+		}
+	}
 
-    foreach ($select->getElements() as $option) {
-        $option->setBody($select->getAttribute('label'));
-        if ($params['selected'] == $option->getAttribute('label')) {
-            $option->setAttribute('selected', 'selected');
-        }
-    }
+	foreach ($select->getElements() as $option) {
+		$option->setBody($select->getAttribute('label'));
+		if ($params['selected'] == $option->getAttribute('label')) {
+			$option->setAttribute('selected', 'selected');
+		}
+	}
 
-    if (C\StringUtils::isBlank($select->getAttribute('name'))) {
-        $options = C\Tuple::create();
-        foreach ($select->getElements() as $option) {
-            $options[] = $option->getContents();
-        }
-        return $options->join('');
-    } else {
-        return $select->getContents();
-    }
+	if (C\StringUtils::isBlank($select->getAttribute('name'))) {
+		$options = C\Tuple::create();
+		foreach ($select->getElements() as $option) {
+			$options[] = $option->getContents();
+		}
+		return $options->join('');
+	} else {
+		return $select->getContents();
+	}
 }
