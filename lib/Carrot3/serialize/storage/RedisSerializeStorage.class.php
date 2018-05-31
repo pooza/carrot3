03be102ef6d5ec007a76945a2d7bd882cf9693eb
Tatsuya Koishi <tkoishi@b-shock.co.jp>
2018-05-31 13:57:40 +0900

    #204

--- lib/Carrot3/serialize/storage/RedisSerializeStorage.class.php
+++ lib/Carrot3/serialize/storage/RedisSerializeStorage.class.php
@@ -1,26 +1,9 @@
 <?php
-/**
- * @package jp.co.b-shock.carrot3
- * @subpackage serialize.storage
- */
-
 namespace Carrot3;
 
-/**
- * Redisシリアライズストレージ
- *
- * @author 小石達也 <tkoishi@b-shock.co.jp>
- */
 class RedisSerializeStorage extends SerializeStorage {
 	private $server;
 
-	/**
-	 * 初期化
-	 *
-	 * @access public
-	 * @param SerializeHandler $handler
-	 * @return bool
-	 */
 	public function initialize (SerializeHandler $handler):bool {
 		parent::initialize($handler);
 		if (!extension_loaded('redis')) {
@@ -32,14 +15,6 @@ class RedisSerializeStorage extends SerializeStorage {
 		return true;
 	}
 
-	/**
-	 * 属性を返す
-	 *
-	 * @access public
-	 * @param string $name 属性の名前
-	 * @param Date $date 比較する日付 - この日付より古い属性値は破棄
-	 * @return mixed 属性値
-	 */
 	public function getAttribute (string $name, Date $date = null) {
 		if ($entry = $this->server[$name]) {
 			if (!$date || !$this->getUpdateDate($name)->isPast($date)) {
@@ -52,13 +27,6 @@ class RedisSerializeStorage extends SerializeStorage {
 		}
 	}
 
-	/**
-	 * 属性を設定
-	 *
-	 * @access public
-	 * @param string $name 属性の名前
-	 * @param mixed $value 値
-	 */
 	public function setAttribute (string $name, $value) {
 		if ($ttl = (int)$this->handler->getConfig('template_cache_ttl')) {
 			$this->server->setEx($name, $ttl, [
@@ -73,42 +41,20 @@ class RedisSerializeStorage extends SerializeStorage {
 		}
 	}
 
-	/**
-	 * 属性を削除
-	 *
-	 * @access public
-	 * @param string $name 属性の名前
-	 */
 	public function removeAttribute (string $name) {
-		return $this->server->delete($name);
+		return $this->server[$name] = null;
 	}
 
-	/**
-	 * 属性の更新日を返す
-	 *
-	 * @access public
-	 * @param string $name 属性の名前
-	 * @return Date 更新日
-	 */
 	public function getUpdateDate (string $name):?Date {
 		if ($entry = $this->server[$name]) {
 			return Date::create($entry['update_date']);
 		}
 	}
 
-	/**
-	 * クリア
-	 *
-	 * @access public
-	 */
 	public function clear () {
 		$this->server->clear();
 	}
 
-	/**
-	 * @access public
-	 * @return string 基本情報
-	 */
 	public function __toString () {
 		return 'Redisシリアライズストレージ';
 	}
